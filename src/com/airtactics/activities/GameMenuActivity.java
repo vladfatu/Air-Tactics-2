900e55be91f3e7f1d58012c699e315b85b5c1b46
Vlad <colegu123@gmail.com>
2014-02-25 01:12:15 +0200

    - worked on integrating play services

--- src/com/airtactics/activities/GameMenuActivity.java
+++ src/com/airtactics/activities/GameMenuActivity.java
@@ -1,6 +1,10 @@
 package com.airtactics.activities;
 
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+
 import airtactics.com.R;
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -10,7 +14,13 @@ import android.widget.Toast;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.games.Games;
+import com.google.android.gms.games.GamesClient;
+import com.google.android.gms.games.multiplayer.realtime.RoomConfig;
+import com.google.android.gms.games.multiplayer.turnbased.TurnBasedMatch;
+import com.google.android.gms.games.multiplayer.turnbased.TurnBasedMatchConfig;
+import com.google.android.gms.games.multiplayer.turnbased.TurnBasedMultiplayer;
 import com.google.example.games.basegameutils.BaseGameActivity;
 
 /**
@@ -20,9 +30,12 @@ import com.google.example.games.basegameutils.BaseGameActivity;
 public class GameMenuActivity extends BaseGameActivity{
 	
 	protected static final int RC_UNUSED = 0;
+	private static final int RC_SELECT_PLAYERS = 5000;
+	private static final int RC_LOOK_AT_MATCHES = 5001;
 	private Button buttonSinglePlayer;
 	private Button buttonOnline;
 	private Button buttonInviteFriends;
+	private Button buttonCheckMatches;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -69,12 +82,111 @@ public class GameMenuActivity extends BaseGameActivity{
 			{
 				Intent intent = Games.TurnBasedMultiplayer.getSelectOpponentsIntent(getApiClient(),
 		                1, 7, true);
-		        startActivityForResult(intent, RC_UNUSED);
+		        startActivityForResult(intent, RC_SELECT_PLAYERS);
+				
+			}
+		});
+		
+		this.buttonCheckMatches = (Button) findViewById(R.id.buttonCheckMatches);
+		this.buttonCheckMatches.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v)
+			{
+				Intent intent = Games.TurnBasedMultiplayer.getInboxIntent(getApiClient());
+		        startActivityForResult(intent, RC_LOOK_AT_MATCHES);
 				
 			}
 		});
 	}
+	
+	public void onActivityResult(int request, int response, Intent data) {
+        super.onActivityResult(request, response, data);
 
+       if (request == RC_LOOK_AT_MATCHES) {
+            // Returning from the 'Select Match' dialog
+
+            if (response != Activity.RESULT_OK) {
+                // user canceled
+                return;
+            }
+
+            TurnBasedMatch match = data
+                    .getParcelableExtra(GamesClient.EXTRA_TURN_BASED_MATCH);
+
+//            if (match != null) {
+//                updateMatch(match);
+//            }
+//
+//            Log.d(TAG, "Match = " + match);
+        } else if (request == RC_SELECT_PLAYERS) {
+            // Returned from 'Select players to Invite' dialog
+
+            if (response != Activity.RESULT_OK) {
+                // user canceled
+                return;
+            }
+
+            // get the invitee list
+            final ArrayList<String> invitees = data
+                    .getStringArrayListExtra(GamesClient.EXTRA_PLAYERS);
+
+            // get automatch criteria
+            Bundle autoMatchCriteria = null;
+
+            int minAutoMatchPlayers = data.getIntExtra(
+                    GamesClient.EXTRA_MIN_AUTOMATCH_PLAYERS, 0);
+            int maxAutoMatchPlayers = data.getIntExtra(
+                    GamesClient.EXTRA_MAX_AUTOMATCH_PLAYERS, 0);
+
+            if (minAutoMatchPlayers > 0) {
+                autoMatchCriteria = RoomConfig.createAutoMatchCriteria(
+                        minAutoMatchPlayers, maxAutoMatchPlayers, 0);
+            } else {
+                autoMatchCriteria = null;
+            }
+
+            TurnBasedMatchConfig tbmc = TurnBasedMatchConfig.builder()
+                    .addInvitedPlayers(invitees)
+                    .setAutoMatchCriteria(autoMatchCriteria).build();
+
+            // Start the match
+            Games.TurnBasedMultiplayer.createMatch(getApiClient(), tbmc).setResultCallback(
+                    new ResultCallback<TurnBasedMultiplayer.InitiateMatchResult>() {
+                @Override
+                public void onResult(TurnBasedMultiplayer.InitiateMatchResult result) {
+                    //processResult(result);
+                	startMatch(result.getMatch());
+                	Toast.makeText(GameMenuActivity.this, "game created ", Toast.LENGTH_SHORT).show();
+                }
+            });
+            //showSpinner();
+        }
+    }
+	
+	public void startMatch(TurnBasedMatch match) {
+//        mTurnData = new SkeletonTurn();
+//        // Some basic turn data
+//        mTurnData.data = "First turn";
+//
+//        mMatch = match;
+
+        String playerId = Games.Players.getCurrentPlayerId(getApiClient());
+        String myParticipantId = match.getParticipantId(playerId);
+
+//        showSpinner();
+
+        Games.TurnBasedMultiplayer.takeTurn(getApiClient(), match.getMatchId(),
+        		"test".getBytes(Charset.forName("UTF-16")), myParticipantId).setResultCallback(
+                new ResultCallback<TurnBasedMultiplayer.UpdateMatchResult>() {
+            @Override
+            public void onResult(TurnBasedMultiplayer.UpdateMatchResult result) {
+            	Toast.makeText(GameMenuActivity.this, "game result upated", Toast.LENGTH_SHORT).show();
+                //processResult(result);
+            }
+        });
+    }
+	
 	@Override
 	public void onSignInFailed()
 	{
